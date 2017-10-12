0cee90c2ff1cce38c39fd6146118048e640ec2d4
Jens Tuyls <you@example.com>
2017-10-11 23:00:08 -0700

    Initialize modals

---
+++ JS/modal.js
@@ -0,0 +1,7 @@
+document.getElementById("signup").addEventListener("click", function( event ) {
+    this.style.visibility = "visible"; 
+})
+
+document.getElementById("login").addEventListener("click", function ( event ) {
+    this.style.visibility = "visible"; 
+}) 
