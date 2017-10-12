c04a315a8e2d1843a1fa0078157e7fc9ec5b7ace
Jens Tuyls <you@example.com>
2017-10-11 23:50:10 -0700

    Finished signup/login page: added login/signup forms and interactivity

--- JS/modal.js
+++ JS/modal.js
@@ -1,7 +1,26 @@
-document.getElementById("signup").addEventListener("click", function( event ) {
-    this.style.visibility = "visible"; 
-})
+document.getElementById("signup-button").addEventListener("click", function( event ) {
+    document.getElementById("signup-modal").classList.remove("hidden"); 
+    document.getElementById("login-modal").classList.add("hidden"); 
+    document.getElementsByClassName("pseudo")[0].style.opacity = 0.9; 
+    document.getElementById("icon-close").classList.remove("hidden");
+    document.getElementById("signup-button").classList.add("hidden");
+    document.getElementById("login-button").classList.add("hidden");
+});
 
-document.getElementById("login").addEventListener("click", function ( event ) {
-    this.style.visibility = "visible"; 
-}) 
+document.getElementById("login-button").addEventListener("click", function ( event ) {
+    document.getElementById("login-modal").classList.remove("hidden");
+    document.getElementById("signup-modal").classList.add("hidden");
+    document.getElementById("icon-close").classList.remove("hidden");
+    document.getElementById("signup-button").classList.add("hidden");
+    document.getElementById("login-button").classList.add("hidden");
+    document.getElementsByClassName("pseudo")[0].style.opacity = 0.9;
+}); 
+
+document.getElementById("icon-close").addEventListener("click", function ( event ) {
+    document.getElementById("login-modal").classList.add("hidden");
+    document.getElementById("signup-modal").classList.add("hidden");
+    document.getElementById("icon-close").classList.add("hidden");
+    document.getElementById("signup-button").classList.remove("hidden");
+    document.getElementById("login-button").classList.remove("hidden");
+    document.getElementsByClassName("pseudo")[0].style.opacity = 0.3;
+});
